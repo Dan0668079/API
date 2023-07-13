0939201bc9315bb912be39318f64c538daf595c1
Daniel Rodrigues <daniel.r.silva@globalhitss.com.br>
2023-07-13 10:36:39 -0300

    estabelecido a conexão com o mongo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
+mongoose.connect("mongodb://localhost:27017").then(() => {
+  const app = express();
+  const port = 3001;
 
-const port = 3001;
-
-app.listen(3001, () => {
-  console.log(`🚀 server is running on http://localhost:${port}`);
+  app.listen(port, () => {
+    console.log(`🚀 server is running on http://localhost:${port}`);
+  });
 });
